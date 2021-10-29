19e1bd5936be2a3d2ee5984118db6e323e3309e9
LAPTOP-93UOEUSO\HP <seuyanlan@outlook.com>
2021-10-29 15:45:21 +0200

    message

--- nbody.py
+++ nbody.py
@@ -8,7 +8,7 @@
 # 2to3
 # modified by Andriy Misyura
 # slightly modified by bmmeijers
-#slightly modified by Lan Yan,Louis Dechamps for Python Assignments
+#slightly modified by Lan Yan,Louis Dechamps 
 
 import sys
 from math import sqrt, pi as PI
